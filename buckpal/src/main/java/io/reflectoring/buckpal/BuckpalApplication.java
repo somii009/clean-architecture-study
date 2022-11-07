036bc323ed680aef434ea5314464323a750128d7
Somi <rlathal009@gmail.com>
2022-11-07 21:51:44 +0900

    Initial Commit

---
+++ buckpal/src/main/java/io/reflectoring/buckpal/BuckpalApplication.java
@@ -0,0 +1,13 @@
+package io.reflectoring.buckpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuckpalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuckpalApplication.class, args);
+	}
+
+}
