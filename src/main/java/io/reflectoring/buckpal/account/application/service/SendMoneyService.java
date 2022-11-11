d16eae75bae0ca631e9a311fbbf7d5970c623a8e
Somi Kim <rlathal009@gmail.com>
2022-11-11 23:45:37 +0900

    feat : sendMoneyService TODO 추가

--- src/main/java/io/reflectoring/buckpal/account/application/service/SendMoneyService.java
+++ src/main/java/io/reflectoring/buckpal/account/application/service/SendMoneyService.java
@@ -5,20 +5,70 @@ import io.reflectoring.buckpal.account.application.port.out.LoadAccountPort;
 import io.reflectoring.buckpal.account.application.port.out.UpdateAccountStatePort;
 import io.reflectoring.buckpal.account.application.port.in.SendMoneyCommand;
 import io.reflectoring.buckpal.account.application.port.in.SendMoneyUseCase;
+import io.reflectoring.buckpal.account.domain.Account;
+import io.reflectoring.buckpal.account.domain.Account.AccountId;
+import io.reflectoring.buckpal.common.Usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
+@Usecase
 @Transactional
 public class SendMoneyService implements SendMoneyUseCase {
     private final LoadAccountPort loadAccountPort;
     private final AccountLock accountLock;
     private final UpdateAccountStatePort updateAccountStatePort;
+    private final MoneyTransferProperties moneyTransferProperties;
 
     @Override
     public boolean sendMoney(SendMoneyCommand command) {
-        //TODO : 비즈니스 규칙 검증
-        //TODO : 모델 상태 조작
-        //TODO : 출력 값 반환
+
+        checkThreshold(command);
+
+        LocalDateTime baselineDate = LocalDateTime.now()
+                .minusDays(10);
+
+        Account sourceAccount = loadAccountPort.loadAccount(
+                command.getSourceAccountId(),
+                baselineDate);
+
+        Account targetAccount = loadAccountPort.loadAccount(
+                command.getTargetAccountId(),
+                baselineDate);
+
+        AccountId sourceAccountId = sourceAccount.getId()
+                .orElseThrow(() -> new IllegalStateException("expected source account ID not to be empty"));
+        AccountId targetAccountId = targetAccount.getId()
+                .orElseThrow(() -> new IllegalStateException("expected target account ID not to be empty"));
+
+        accountLock.lockAccount(sourceAccountId);
+        if (!sourceAccount.withdraw(command.getMoney(), targetAccountId)) {
+            accountLock.releaseAccount(sourceAccountId);
+            return false;
+        }
+
+        accountLock.lockAccount(targetAccountId);
+        if (!targetAccount.deposit(command.getMoney(), sourceAccountId)) {
+            accountLock.releaseAccount(sourceAccountId);
+            accountLock.releaseAccount(targetAccountId);
+            return false;
+        }
+
+        updateAccountStatePort.updateActivities(sourceAccount);
+        updateAccountStatePort.updateActivities(targetAccount);
+
+        accountLock.releaseAccount(sourceAccountId);
+        accountLock.releaseAccount(targetAccountId);
+        return true;
+    }
+
+    private void checkThreshold(SendMoneyCommand command) {
+        if (command.getMoney()
+                .isGreaterThan(moneyTransferProperties.getMaximumTransferThreshold())) {
+            throw new ThresholdExceededException(moneyTransferProperties.getMaximumTransferThreshold(),
+                    command.getMoney());
+        }
     }
 }
