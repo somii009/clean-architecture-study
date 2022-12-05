3e23c4ff458af5742d4b514f0d5d508c032b6035
Somi Kim <rlathal009@gmail.com>
2022-12-05 23:22:06 +0900

    feat : SendMoneyController 추가

---
+++ src/main/java/io/reflectoring/buckpal/account/adapter/in/web/SendMoneyController.java
@@ -0,0 +1,30 @@
+package io.reflectoring.buckpal.account.adapter.in.web;
+import io.reflectoring.buckpal.account.application.port.in.SendMoneyCommand;
+import io.reflectoring.buckpal.account.application.port.in.SendMoneyUseCase;
+import io.reflectoring.buckpal.account.domain.Account.AccountId;
+import io.reflectoring.buckpal.account.domain.Money;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class SendMoneyController {
+
+    private final SendMoneyUseCase sendMoneyUseCase;
+
+    @PostMapping(path = "/accounts/send/{sourceAccountId}/{targetAccountId}/{amount}")
+    void sendMoney(
+            @PathVariable("sourceAccountId") Long sourceAccountId,
+            @PathVariable("targetAccountId") Long targetAccountId,
+            @PathVariable("amount") Long amount) {
+
+        SendMoneyCommand command = new SendMoneyCommand(
+                new AccountId(sourceAccountId),
+                new AccountId(targetAccountId),
+                Money.of(amount));
+
+        sendMoneyUseCase.sendMoney(command);
+    }
+}
